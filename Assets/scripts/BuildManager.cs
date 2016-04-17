7ede3a86f0fdd6de21344884e952f6fdb5974440
ArturNemirko <arturnemirko@mail.ru>
2016-04-17 23:29:18 +0200

    #3

--- Assets/scripts/BuildManager.cs
+++ Assets/scripts/BuildManager.cs
@@ -6,49 +6,50 @@ public class BuildManager : MonoBehaviour {
 	public GameObject towerSmall;
 	public GameObject towerMedium;
 	public GameObject towerWall;
+	private GameObject tower;
+	private Vector3 curPos;
 	
 	void Update () {
 	
-		if (Input.GetMouseButtonDown (0)) {
-			Ray r = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-			RaycastHit rh;
-			if(Physics.Raycast(r, out rh, Mathf.Infinity))
-			{
-				if(rh.transform.gameObject.tag == "Platform" && !rh.transform.gameObject.GetComponent<Platform>().checkTower)
-				{
-					switch(GlobalVars.towerSelected)
-					{
-					case GlobalVars.ClickState.TowerSmall:
-						if(TowerSmall.towerPrice <= GlobalVars.PlayerMoney)
-						{
-							GameObject towerS = Instantiate(towerSmall, new Vector3(rh.transform.position.x, rh.transform.position.y + 0.75f, rh.transform.position.z), Quaternion.identity) as GameObject;
-							GlobalVars.PlayerMoney -= TowerSmall.towerPrice;
-							rh.transform.gameObject.GetComponent<Platform>().checkTower = true;
-							towerS.GetComponent<TowerSmall>().plt = rh.transform.gameObject.GetComponent<Platform>();
-						}
-						break;
-					case GlobalVars.ClickState.TowerMedium :
-						if(TowerMedium.towerPrice <= GlobalVars.PlayerMoney)
-						{
-							GameObject towerM = Instantiate(towerMedium, new Vector3(rh.transform.position.x, rh.transform.position.y + 1.25f, rh.transform.position.z), Quaternion.identity) as GameObject;
-							GlobalVars.PlayerMoney -= TowerMedium.towerPrice;
-							rh.transform.gameObject.GetComponent<Platform>().checkTower = true;
-							towerM.GetComponent<TowerMedium>().plt = rh.transform.gameObject.GetComponent<Platform>();
-						}
-						break;
-					case GlobalVars.ClickState.TowerWall :
-						if(TowerWall.towerPrice <= GlobalVars.PlayerMoney)
-						{
-							GameObject towerW = Instantiate(towerWall, new Vector3(rh.transform.position.x, rh.transform.position.y + 1.25f, rh.transform.position.z), Quaternion.identity) as GameObject;
-							GlobalVars.PlayerMoney -= TowerWall.towerPrice;
-							rh.transform.gameObject.GetComponent<Platform>().checkTower = true;
-							towerW.GetComponent<TowerWall>().plt = rh.transform.gameObject.GetComponent<Platform>();
-						}
-						break;
-					}
-				}
+		Ray r = Camera.main.ScreenPointToRay(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
+		RaycastHit rh;
+		if (Physics.Raycast (r, out rh)) {
+			curPos = rh.point;
+		}
+
+		if (tower != null && Input.GetMouseButtonDown (0)) {						
+			tower = null;
+		}
+		if (tower != null) {
+			tower.transform.position = new Vector3 (curPos.x, 1, curPos.z);
+		}
+
+
+		switch (GlobalVars.towerSelected) {
+		case GlobalVars.ClickState.TowerSmall:
+			if (TowerSmall.towerPrice <= GlobalVars.PlayerMoney && tower == null) {
+				tower = Instantiate (towerSmall, new Vector3 (curPos.x, 1, curPos.z), Quaternion.identity) as GameObject;
+				GlobalVars.PlayerMoney -= TowerSmall.towerPrice;
+				GlobalVars.towerSelected = GlobalVars.ClickState.None;
 
 			}
+			break;
+		case GlobalVars.ClickState.TowerMedium:
+			if (TowerMedium.towerPrice <= GlobalVars.PlayerMoney && tower == null) {
+				tower = Instantiate (towerMedium, new Vector3 (curPos.x, 1, curPos.z), Quaternion.identity) as GameObject;
+				GlobalVars.PlayerMoney -= TowerMedium.towerPrice;
+				GlobalVars.towerSelected = GlobalVars.ClickState.None;
+
+			}
+			break;
+		case GlobalVars.ClickState.TowerWall :
+			if(TowerWall.towerPrice <= GlobalVars.PlayerMoney && tower == null){
+				tower = Instantiate(towerWall, new Vector3 (curPos.x, 1, curPos.z), Quaternion.identity) as GameObject;
+				GlobalVars.PlayerMoney -= TowerWall.towerPrice;
+				GlobalVars.towerSelected = GlobalVars.ClickState.None;
+
+			}
+			break;
 		}
 	}
 }
--- Assets/scripts/UInterface.cs
+++ Assets/scripts/UInterface.cs
@@ -3,34 +3,37 @@ using System.Collections;
 
 public class UInterface : MonoBehaviour
 {
-	
+
 	public Rect buyMenu; 
+	public Texture TS;
+	public Texture TM;
+	public Texture TW;
 	public Rect towerSmall;
 	public Rect towerMedium; 
 	public Rect towerWall; 
 	public Rect playerStats; 
 	public Rect playerStatsPlayerMoney; 
-	
+
 	public GameObject plasmaTower;
 	public GameObject plasmaTowerGhost; 
 
 	private RaycastHit hit;
 	public LayerMask raycastLayers = 1; 
-	
+
 	//private GameObject ghost;
 	private void Awake()
 	{
-		
-		buyMenu = new Rect(Screen.width - 185.0f, 10.0f, 175.0f, Screen.height - 100.0f);  
-		towerSmall = new Rect(buyMenu.x + 12.5f, buyMenu.y + 30.0f, 150.0f, 50.0f);
-		towerMedium = new Rect(towerSmall.x, buyMenu.y + 90.0f, 150.0f, 50.0f);
-		towerWall = new Rect(towerSmall.x, buyMenu.y + 150.0f, 150.0f, 50.0f);
+
+		buyMenu = new Rect(Screen.width - 65.0f, 10.0f, 65.0f, Screen.height - 110.0f);  
+		towerSmall = new Rect(buyMenu.x + 6.5f, buyMenu.y + 35.0f, 50.0f, 50.0f);
+		towerMedium = new Rect(towerSmall.x, buyMenu.y + 90.0f, 50.0f, 50.0f);
+		towerWall = new Rect(towerSmall.x, buyMenu.y + 145.0f, 50.0f, 50.0f);
 
 		playerStats = new Rect(10.0f, 10.0f, 150.0f, 100.0f);
 		playerStatsPlayerMoney = new Rect(playerStats.x + 12.5f, playerStats.y + 30.0f, 125.0f, 25.0f);
-		
+
 	}
-	
+
 	private void Update()
 	{
 		if(Input.GetMouseButtonDown(1))
@@ -38,25 +41,25 @@ public class UInterface : MonoBehaviour
 			GlobalVars.towerSelected = GlobalVars.ClickState.None;
 		}
 	}
-	
+
 	private void OnGUI()
 	{
-		GUI.Box(buyMenu, "Buying menu"); 
-		if (GUI.Button(towerSmall, "Tower Small\n" + TowerSmall.towerPrice.ToString() + " ДНК"))
+		GUI.Box(buyMenu, "Buying\n menu"); 
+		if (GUI.Button(towerSmall, TS))//"Tower Small\n" + TowerSmall.towerPrice.ToString() + " ДНК"))
 		{
 			GlobalVars.towerSelected = GlobalVars.ClickState.TowerSmall;
 
 		}
-		else if (GUI.Button(towerMedium, "Tower Medium\n" + TowerMedium.towerPrice.ToString() + " ДНК"))
+		else if (GUI.Button(towerMedium, TM)) //"Tower Medium\n" + TowerMedium.towerPrice.ToString() + " ДНК"))
 		{
 			GlobalVars.towerSelected = GlobalVars.ClickState.TowerMedium;
 		}
-		else if (GUI.Button(towerWall, "Tower Wall\n" + TowerWall.towerPrice.ToString() + " ДНК"))
+		else if (GUI.Button(towerWall, TW))//"Tower Wall\n" + TowerWall.towerPrice.ToString() + " ДНК"))
 		{
 			GlobalVars.towerSelected = GlobalVars.ClickState.TowerWall;
 		}
 
-		
+
 		GUI.Box(playerStats, "Player Stats");
 		GUI.Label(playerStatsPlayerMoney, "ДНК: " + GlobalVars.PlayerMoney);
 
