8e6fc823d8636b0991b3de79e2740dac68effd1d
Pavel Evleev <mr.malefiic@gmail.com>
2016-04-29 23:02:01 +0300

    #26 [CODE] Интерфейс игрового поля
    почти все сделал, осталось привязать к чему-то полоску волны

---
+++ Assets/scripts/ButtenDefenders.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtenDefenders : MonoBehaviour {
+
+	private void Update()
+	{
+		if(Input.GetMouseButtonDown(1))
+		{
+			GlobalVars.towerSelected = GlobalVars.ClickState.None;
+		}
+	}
+	public void SelectedTowerSmall () {
+		GlobalVars.towerSelected = GlobalVars.ClickState.TowerSmall;
+	}
+	public void SelectedTowerMedium () {
+		GlobalVars.towerSelected = GlobalVars.ClickState.TowerMedium;
+	}
+	public void SelectedTowerWall () {
+		GlobalVars.towerSelected = GlobalVars.ClickState.TowerWall;
+	}
+}
