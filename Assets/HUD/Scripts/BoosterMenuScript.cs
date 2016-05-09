1421457e73de67426c2a001d9583c517d6229768
Art.Stea1th (Станислав Кузьмич) <art.stea1th@gmail.com>
2016-05-09 15:35:23 +0300

    added: simple BoosterMenu animation #33

---
+++ Assets/HUD/Scripts/BoosterMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+
+public class BoosterMenuScript : MonoBehaviour {
+
+    private bool currentMenuState;
+
+    public Animator boosterMenu;
+
+    public void Execute() {
+        boosterMenu.SetBool("isHidden", currentMenuState);
+        currentMenuState = !currentMenuState;
+    }
+
+    public BoosterMenuScript() {
+        currentMenuState = false;
+    }
+}
