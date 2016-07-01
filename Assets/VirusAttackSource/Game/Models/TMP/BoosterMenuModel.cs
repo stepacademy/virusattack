9f5cf5bffbb05c0c200ddab334ab2f48ac1c55a8
Art.Stea1th <art.stea1th@gmail.com>
2016-07-01 19:50:37 +0300

    hotfix: GUI, revert support for all screen ratios, revert simple animation

---
+++ Assets/VirusAttackSource/Game/Models/TMP/BoosterMenuModel.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using Assets.VirusAttackSource.AMVCC;
+
+namespace Assets.VirusAttackSource.Game.Models.TMP {
+
+    public class BoosterMenuModel : Model<VirusAttack> {
+
+        private bool currentMenuState;
+
+        [SerializeField] private Animator _boosterMenu;
+
+        public void Execute() {
+            _boosterMenu.SetBool("isHidden", currentMenuState);
+            currentMenuState = !currentMenuState;
+        }
+
+        public BoosterMenuModel() {
+            currentMenuState = false;
+        }
+    }
+
+}
